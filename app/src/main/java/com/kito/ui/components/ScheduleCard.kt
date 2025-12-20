198cdd6801cc6743f940c9f877c5cce243635f15
Pratyusha Mohanty <pratyushamohanty200905@gmail.com>
2025-12-21 03:46:52 +0530

    -Home Screen Implementation

--- app/src/main/java/com/kito/ui/components/ScheduleCard.kt
+++ app/src/main/java/com/kito/ui/components/ScheduleCard.kt
@@ -3,15 +3,9 @@ package com.kito.ui.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -26,51 +20,44 @@ fun ScheduleCard(colors: UIColors) {
                 colors.cardBackground,
                 RoundedCornerShape(22.dp)
             )
-            .padding( horizontal = 16.dp, vertical = 8.dp)
+            .padding(16.dp)
     ) {
+        Text(
+            text = "My Schedule",
+            color = colors.textPrimary,
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 20.sp,
+            fontFamily = FontFamily.Monospace
+        )
+
+        Spacer(Modifier.height(12.dp))
+
         ScheduleItem("AI A-201", "8:00 - 8:30", colors)
         ScheduleItem("ML B-102", "8:30 - 9:00", colors)
-        ScheduleItem("UHV C-101", "10:00 - 10:30", colors)
+        ScheduleItem("Universal Human Values C-101", "10:00 - 10:30", colors)
         ScheduleItem("Extra", "12:00 - 12:30", colors)
     }
 }
 
-@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
-fun ScheduleItem(
+private fun ScheduleItem(
     title: String,
     time: String,
     colors: UIColors
 ) {
-    Row(
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Box(
-            modifier = Modifier
-                .width(4.dp)
-                .height(32.dp)
-                .background(
-                    Brush.verticalGradient(
-                        listOf(colors.accentOrangeStart, colors.accentOrangeEnd)
-                    ),
-                    RoundedCornerShape(2.dp)
-                )
+    Column(modifier = Modifier.padding(vertical = 6.dp)) {
+        Text(
+            text = title,
+            color = colors.textPrimary,
+            fontWeight = FontWeight.Bold,
+            fontFamily = FontFamily.Monospace,
+            fontSize = 18.sp,
         )
-        Spacer(modifier = Modifier.width(8.dp))
-        Column(modifier = Modifier.padding(vertical = 6.dp)) {
-            Text(
-                text = title,
-                color = colors.textPrimary,
-                fontWeight = FontWeight.Bold,
-                fontFamily = FontFamily.Monospace,
-                style = MaterialTheme.typography.labelLargeEmphasized
-            )
-            Text(
-                text = time,
-                color = colors.textPrimary.copy(alpha = 0.85f),
-                style = MaterialTheme.typography.labelSmallEmphasized,
-                fontFamily = FontFamily.Monospace
-            )
-        }
+        Text(
+            text = time,
+            color = colors.textPrimary.copy(alpha = 0.85f),
+            fontSize = 16.sp,
+            fontFamily = FontFamily.Monospace
+        )
     }
 }
