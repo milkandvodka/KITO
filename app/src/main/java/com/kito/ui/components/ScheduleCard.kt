4a42f41b58566878590c945ed7efabbff9c97f4a
ShanuDevCodes <shanu.dev.codes@gmail.com>
2025-12-21 06:08:37 +0530

    -Implemented Home Screen

--- app/src/main/java/com/kito/ui/components/ScheduleCard.kt
+++ app/src/main/java/com/kito/ui/components/ScheduleCard.kt
@@ -3,9 +3,15 @@ package com.kito.ui.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -20,44 +26,51 @@ fun ScheduleCard(colors: UIColors) {
                 colors.cardBackground,
                 RoundedCornerShape(22.dp)
             )
-            .padding(16.dp)
+            .padding( horizontal = 16.dp, vertical = 8.dp)
     ) {
-        Text(
-            text = "My Schedule",
-            color = colors.textPrimary,
-            fontWeight = FontWeight.SemiBold,
-            fontSize = 20.sp,
-            fontFamily = FontFamily.Monospace
-        )
-
-        Spacer(Modifier.height(12.dp))
-
         ScheduleItem("AI A-201", "8:00 - 8:30", colors)
         ScheduleItem("ML B-102", "8:30 - 9:00", colors)
-        ScheduleItem("Universal Human Values C-101", "10:00 - 10:30", colors)
+        ScheduleItem("UHV C-101", "10:00 - 10:30", colors)
         ScheduleItem("Extra", "12:00 - 12:30", colors)
     }
 }
 
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
-private fun ScheduleItem(
+fun ScheduleItem(
     title: String,
     time: String,
     colors: UIColors
 ) {
-    Column(modifier = Modifier.padding(vertical = 6.dp)) {
-        Text(
-            text = title,
-            color = colors.textPrimary,
-            fontWeight = FontWeight.Bold,
-            fontFamily = FontFamily.Monospace,
-            fontSize = 18.sp,
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Box(
+            modifier = Modifier
+                .width(4.dp)
+                .height(32.dp)
+                .background(
+                    Brush.verticalGradient(
+                        listOf(colors.accentOrangeStart, colors.accentOrangeEnd)
+                    ),
+                    RoundedCornerShape(2.dp)
+                )
         )
-        Text(
-            text = time,
-            color = colors.textPrimary.copy(alpha = 0.85f),
-            fontSize = 16.sp,
-            fontFamily = FontFamily.Monospace
-        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Column(modifier = Modifier.padding(vertical = 6.dp)) {
+            Text(
+                text = title,
+                color = colors.textPrimary,
+                fontWeight = FontWeight.Bold,
+                fontFamily = FontFamily.Monospace,
+                style = MaterialTheme.typography.labelLargeEmphasized
+            )
+            Text(
+                text = time,
+                color = colors.textPrimary.copy(alpha = 0.85f),
+                style = MaterialTheme.typography.labelSmallEmphasized,
+                fontFamily = FontFamily.Monospace
+            )
+        }
     }
 }
--- app/src/main/java/com/kito/ui/newUi/screen/HomeScreen.kt
+++ app/src/main/java/com/kito/ui/newUi/screen/HomeScreen.kt
@@ -1,93 +1,186 @@
 package com.kito.ui.newUi.screen
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowForwardIos
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.kito.ui.components.*
 
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 fun HomeScreen(
-    username: String,
-    colors: UIColors = UIColors()
 ) {
-    val bgGradient = Brush.verticalGradient(
-        colors = listOf(colors.backgroundTop, colors.backgroundBottom)
-    )
-
-    LazyColumn(
+    val uiColors = UIColors()
+    Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(bgGradient)
-            .padding(16.dp)
+            .padding(horizontal = 12.dp)
     ) {
-
-        item {
-            Text(
-                text = "Hi ABC 👋",
-                color = colors.textPrimary,
-                fontSize = 22.sp,
-                fontWeight = FontWeight.Bold,
-                fontFamily = FontFamily.Monospace
-            )
-
-            Spacer(Modifier.height(20.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+            ) {
+                Text(
+                    text = "Welcome",
+                    color = uiColors.progressAccent,
+                    fontWeight = FontWeight.SemiBold,
+                    fontFamily = FontFamily.Monospace,
+                    style = MaterialTheme.typography.titleLargeEmphasized
+                )
+                Text(
+                    text = "Pratyusha 👋",
+                    color = uiColors.textPrimary,
+                    fontWeight = FontWeight.Bold,
+                    fontFamily = FontFamily.Monospace,
+                    style = MaterialTheme.typography.displaySmallEmphasized
+                )
+            }
+//            IconButton(
+//                onClick = {}
+//            ) {
+//                Icon(
+//                    imageVector = Icons.Default.Notifications,
+//                    contentDescription = "Notifications",
+//                    tint = uiColors.textPrimary
+//                )
+//            }
         }
-        item {
-            Text(
-                text = "Dashboard",
-                color = colors.textPrimary,
-                fontSize = 22.sp,
-                fontWeight = FontWeight.Bold,
-                fontFamily = FontFamily.Monospace
-            )
+        LazyColumn() {
+            item {
+                Spacer(modifier = Modifier.height(8.dp))
+            }
 
-            Spacer(Modifier.height(20.dp))
-        }
+            item {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = "Today's Schedule",
+                        color = uiColors.textPrimary,
+                        fontWeight = FontWeight.Bold,
+                        fontFamily = FontFamily.Monospace,
+                        style = MaterialTheme.typography.titleMedium,
+                        modifier = Modifier.weight(1f)
+                    )
+                    IconButton(
+                        onClick = {},
+                        modifier = Modifier.size(28.dp)
+                    ) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Default.ArrowForwardIos,
+                            contentDescription = "Notifications",
+                            tint = uiColors.textPrimary,
+                            modifier = Modifier.size(16.dp)
+                        )
+                    }
+                }
+            }
 
-        // Schedule Section
-        item {
-            ScheduleCard(colors)
-            Spacer(Modifier.height(24.dp))
-        }
-//        item {
-//            Text(
-//                text = "",
-//                color = colors.textPrimary,
-//                fontSize = 22.sp,
-//                fontWeight = FontWeight.Bold,
-//                fontFamily = FontFamily.Monospace
-//            )
-//            Spacer(Modifier.height(20.dp))
-//        }
-        // Horizontal Cards
-        item {
-            DashboardCard(
-                data = DashboardCardData(
-                    title = "Attendance",
-                    subtitle = "Track your presence"
-                ),
-                colors = colors
-            )
+            item {
+                Spacer(Modifier.height(8.dp))
+            }
 
-            Spacer(Modifier.height(16.dp))
+            // Schedule Section
+            item {
+                ScheduleCard(uiColors)
+            }
 
-            DashboardCard(
-                data = DashboardCardData(
-                    title = "Upcoming Events...",
-                    subtitle = "Stay updated"
-                ),
-                colors = colors
-            )
+            item {
+                Spacer(Modifier.height(8.dp))
+            }
+
+            item {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = "Upcoming KIIT Events",
+                        color = uiColors.textPrimary,
+                        fontWeight = FontWeight.Bold,
+                        fontFamily = FontFamily.Monospace,
+                        style = MaterialTheme.typography.titleMedium,
+                        modifier = Modifier.weight(1f)
+                    )
+                    IconButton(
+                        onClick = {},
+                        modifier = Modifier.size(28.dp)
+                    ) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Default.ArrowForwardIos,
+                            contentDescription = "Notifications",
+                            tint = uiColors.textPrimary,
+                            modifier = Modifier.size(16.dp)
+                        )
+                    }
+                }
+            }
+
+            item {
+                Spacer(Modifier.height(8.dp))
+            }
+
+            item {
+                UpcomingEventCard()
+            }
+
+            item {
+                Spacer(Modifier.height(8.dp))
+            }
+
+            item {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = "Overall Attendance",
+                        color = uiColors.textPrimary,
+                        fontWeight = FontWeight.Bold,
+                        fontFamily = FontFamily.Monospace,
+                        style = MaterialTheme.typography.titleMedium,
+                        modifier = Modifier.weight(1f)
+                    )
+                    IconButton(
+                        onClick = {},
+                        modifier = Modifier.size(28.dp)
+                    ) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Default.ArrowForwardIos,
+                            contentDescription = "Notifications",
+                            tint = uiColors.textPrimary,
+                            modifier = Modifier.size(16.dp)
+                        )
+                    }
+                }
+            }
+
+            item {
+                Spacer(Modifier.height(8.dp))
+            }
+
+            // Horizontal Cards
+            item {
+                OverallAttendanceCard(
+                    colors = uiColors
+                )
+            }
+
+            item {
+                Spacer(Modifier.height(8.dp))
+            }
         }
     }
 }
