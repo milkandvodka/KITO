f493ce34047fab4ba288723f2eac97aeb9c83ffc
ShanuDevCodes <shanu.dev.codes@gmail.com>
2025-12-22 00:46:55 +0530

    -Minor Bug Fixe

--- app/src/main/java/com/kito/ui/components/CalenderMonthUI.kt
+++ app/src/main/java/com/kito/ui/components/CalenderMonthUI.kt
@@ -6,10 +6,13 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
@@ -182,7 +185,11 @@ fun CalendarMonthUI(
                         }
                     }
                 }
-                Spacer(modifier = Modifier.height(8.dp))
+                Spacer(
+                    modifier = Modifier.height(
+                        66.dp + WindowInsets.statusBars.asPaddingValues().calculateBottomPadding()
+                    )
+                )
             }
 //        LazyVerticalGrid(
 //            modifier = Modifier.fillMaxSize(),
