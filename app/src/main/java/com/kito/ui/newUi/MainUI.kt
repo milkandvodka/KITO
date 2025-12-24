0b893aae68456b67a366896585ac3e6b4b0fc6cb
Pratyusha Mohanty <pratyushamohanty200905@gmail.com>
2025-12-25 00:38:40 +0530

    -Setting Screen Basic Implementation and Elabs About Dialog Box Implementation

--- app/src/main/java/com/kito/ui/newUi/MainUI.kt
+++ app/src/main/java/com/kito/ui/newUi/MainUI.kt
@@ -30,6 +30,7 @@ import com.kito.ui.navigation.BottomNavigationItems
 import com.kito.ui.navigation.Destinations
 import com.kito.ui.newUi.screen.AttendanceListScreen
 import com.kito.ui.newUi.screen.HomeScreen
+import com.kito.ui.newUi.screen.SettingsScreen
 import com.kito.ui.screen.CalendarScreen
 import dev.chrisbanes.haze.ExperimentalHazeApi
 import dev.chrisbanes.haze.HazeInputScale
@@ -117,7 +118,7 @@ fun MainUI() {
                         AttendanceListScreen()
                     }
                     composable<Destinations.Profile> {
-                        CalendarScreen()
+                        SettingsScreen()
                     }
                 }
             }
