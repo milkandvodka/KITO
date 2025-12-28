0d8261e70fd628613d5b24f06cd16c6ccef97a92
ShanuDevCodes <shanu.dev.codes@gmail.com>
2025-12-28 20:20:35 +0530

    -Minor Bug Fix

--- app/src/main/java/com/kito/ui/components/settingsdialog/RequiredAttendanceDialogBox.kt
+++ app/src/main/java/com/kito/ui/components/settingsdialog/RequiredAttendanceDialogBox.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Balance
-import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
