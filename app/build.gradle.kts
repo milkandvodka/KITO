b15f4f23bb1ab6b7519473e0ed8918a13694bc1d
ShanuDevCodes <shanu.dev.codes@gmail.com>
2025-12-30 01:18:42 +0530

    -Minor Bug Fix -Home Page Schedule Fixed -Haptics Improved

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.kito"
         minSdk = 24
         targetSdk = 35
-        versionCode = 17
-        versionName = "2.1.1"
+        versionCode = 18
+        versionName = "2.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
